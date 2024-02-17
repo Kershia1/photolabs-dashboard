e0a3cbe9534b3a64551fb436cd8c1176856cb22b
Kershia1 <insatiable.meerkat@gmail.com>
2024-02-16 19:22:15 -0800

    odified

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,132 +4,111 @@ import classnames from "classnames";
 import Loading from "./Loading";
 import Panel from "./Panel";
 
+import {
+  getTotalPhotos,
+  getTotalTopics,
+  getUserWithMostUploads,
+  getUserWithLeastUploads
+} from "helpers/selectors";
+
 //Mock Data
 const data = [
   {
     id: 1,
     label: "Total Photos",
-    value: 10
+    getValue: getTotalPhotos
   },
   {
     id: 2,
     label: "Total Topics",
-    value: 4
+    getValue: getTotalTopics
   },
   {
     id: 3,
     label: "User with the most uploads",
-    value: "Allison Saeng"
+    getValue: getUserWithMostUploads
   },
   {
     id: 4,
     label: "User with the least uploads",
-    value: "Lukas Souza"
+    getValue: getUserWithLeastUploads
   }
 ];
 
 
-function Dashboard(props) {
-  const [state, setState] = React.useState({ focused:null});
-
-    selectPanel(id){
-    this.setState({
-     focused: id
-    });
-   }
-}
-
-// class Dashboard extends Component {
+class Dashboard extends Component {
   state = {
-    loading: false,
-    focused: null // ? maybe track a specific panel(id) that is clicked? then
-    //use that to change the state of the panel clicked? toggles to each id
-  }
-
-  render() {
-    const dashboardClasses = classnames("dashboard", {
-      //if focused is true, then apply the class "dashboard--focused"
-      "dashboard--focused": this.state.focused
-    });
-
-
-    if (this.state.loading) {
-      return <Loading />;
-    }
-
-      //Use the this.state.focused value to filter panel data before 
-      //converting it to components.
-
-    // const panels = data.map(dataMock => {
-
-    //   return <Panel key={dataMock.id} id={dataMock.id} label={dataMock.label} value={dataMock.value} />;
-    // })
-
-//If this.state.focused is null then return true for every panel.
-// If this.state.focused is equal to the Panel, then let it through the filter.
-    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
-   .map(panel => (
-    <Panel
-     key={panel.id}
-     id={panel.id}
-     label={panel.label}
-     value={panel.value}
-     onSelect={event => this.selectPanel(panel.id)}// we are passing a reference to the instance method as a prop.
-    />
-   ));
-
-    return <main className={dashboardClasses}>
-      {panels}
-    </main>;
-  }
-
-export default Dashboard;
-
-
-
-  /** Map over the data array and create a new Panel for each of the four data objects. Render the panels array as children of the main element..
-   * 
-   * essential from panels as     // const label = this.props.label;
-    // const value = this.props.value;
-    //const key = this.props.id
-    //cont id = this.props.id
-  */
-
-
-/**
- * Compass answer class Dashboard extends Component {
-  state = {
-    loading: false
+    loading: true,//changed from true to false
+    focused: null
+    photos: [],
+    topics: []
   };
 
-  render() {
-    const dashboardClasses = classnames("dashboard");
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  }
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
 
-    if (this.state.loading) {
-      return <Loading />;
+    if (focused) {
+      this.setState({ focused });
     }
+  }
 
-    const panels = data.map(panel => (
+  const urlsPromise = [
+    "/api/photos",
+    "/api/topics",
+  ].map(url => fetch(url).then(response => response.json()));
+
+    Promise.all(urlsPromise)
+    .then(([photos, topics]) => {
+  this.setState({
+    loading: false,
+    photos: photos,
+    topics: topics
+  });
+});
+}
+
+
+componentDidUpdate(previousProps, previousState) {
+  if (previousState.focused !== this.state.focused) {
+    localStorage.setItem("focused", JSON.stringify(this.state.focused));
+  }
+}
+
+render() {
+  const dashboardClasses = classnames("dashboard", {
+    "dashboard--focused": this.state.focused
+  });
+
+  console.log(this.state);
+
+  if (this.state.loading) {
+    return <Loading />;
+  }
+
+  const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+    .map(panel => (
       <Panel
         key={panel.id}
-        id={panel.id}
         label={panel.label}
-        value={panel.value}
+        value={panel.getValue(this.state)}
+        onSelect={() => this.selectPanel(panel.id)}
       />
     ));
 
-    return <main className={dashboardClasses}>{panels}</main>;
+  if (this.state.loading) {
+    return <Loading />;
   }
-}
 
-
-/**
- * equivalent to:
- * function Dashboard(props) {
-  const dashboardClasses = classnames("dashboard");
-  return <main className={dashboardClasses} />;
+  return (<main className={dashboardClasses}>
+    {panels}
+  </main>
+  );
 }
- * 
- react tutorial 
-https://stackoverflow.com/questions/41027663/how-to-map-an-array-of-objects-in-react
- */
+};
+
+export default Dashboard;
